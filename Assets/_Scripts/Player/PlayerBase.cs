1d71c78355efddc8386ba70128cff5bb8fb50894
Lukáš Urban <lukas.urbann@seznam.cz>
2025-03-15 00:28:46 +0100

    player variants

--- Assets/_Scripts/Player/PlayerBase.cs
+++ Assets/_Scripts/Player/PlayerBase.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using GJ25.Grid;
+using GJ25.Player;
 
 public class PlayerBase : MonoBehaviour
 {
@@ -15,6 +16,13 @@ public class PlayerBase : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float rotSpeed = 20f;
     private Quaternion targetRotation;
+    private PlayerControls _controls;
+
+
+    private void OnEnable()
+    {
+        _controls = GetComponent<PlayerControls>();
+    }
 
     private void Update()
     {
@@ -36,10 +44,10 @@ public class PlayerBase : MonoBehaviour
     {
         int dx = 0, dy = 0;
 
-        if (Input.GetKeyDown(KeyCode.W)) dy = 1;
-        else if (Input.GetKeyDown(KeyCode.S)) dy = -1;
-        else if (Input.GetKeyDown(KeyCode.A)) dx = -1;
-        else if (Input.GetKeyDown(KeyCode.D)) dx = 1;
+        if(Input.GetKeyDown(_controls.up)) dy = 1;
+        if(Input.GetKeyDown(_controls.down)) dy = -1;
+        if(Input.GetKeyDown(_controls.left)) dx = -1;
+        if (Input.GetKeyDown(_controls.right)) dx = 1;
 
         if (dx != 0 || dy != 0)
         {
---
+++ Assets/_Scripts/Player/PlayerControls.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace GJ25.Player
+{
+    public class PlayerControls : MonoBehaviour
+    {
+        public KeyCode up;
+        public KeyCode left;
+        public KeyCode right;
+        public KeyCode down;
+        public KeyCode interact;
+    }
+}
