e666df87b5e0af94967311753e8c8eda6e5584d4
vnmls <162630226+vnmls@users.noreply.github.com>
2025-03-15 18:59:33 +0100

    Add audio manager

--- Assets/_Scripts/Interactables/Chair.cs
+++ Assets/_Scripts/Interactables/Chair.cs
@@ -1,7 +1,15 @@
+using UnityEngine;
+
+
 namespace GJ25.Interactables
 {
     public class Chair : InteractableObjectBase
     {
-
+        public void Test()
+        {
+            Debug.Log("Testing .." + this.name);
+            AudioManager audioManager = FindFirstObjectByType<AudioManager>();
+            audioManager.Play("Test");
+        }
     }
 }
