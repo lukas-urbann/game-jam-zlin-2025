e56256103e9f2d8600fc38a5fc4db74ffe5635f3
Ondrator <162739617+Ondrator@users.noreply.github.com>
2025-03-15 13:21:35 +0100

    Vsechny basic veci

---
+++ Assets/Ondra a Vitek/loadingBar.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loadingBar : MonoBehaviour
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private int minValue = 500;
+    [SerializeField] RectMask2D mask;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            EvaluateSlider(Random.Range(0f,1f));
+        }
+    }
+
+    public void EvaluateSlider(float percentage)
+    {
+        float fillAmount = (1 - percentage)  *  minValue;
+        Debug.Log(fillAmount);
+        mask.padding = new Vector4(0,0, fillAmount, 0);
+    }
+}
