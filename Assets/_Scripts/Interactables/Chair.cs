a2b8a5c40683c32044982d7df763454ac72a12b5
Lukáš Urban <lukas.urbann@seznam.cz>
2025-03-15 17:31:19 +0100

    Update Chair.cs

--- Assets/_Scripts/Interactables/Chair.cs
+++ Assets/_Scripts/Interactables/Chair.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using GJ25.Player;
 using UnityEngine;
 
@@ -5,14 +6,10 @@ namespace GJ25.Interactables
 {
     public class Chair : InteractableObjectBase
     {
-        public void TestChair()
-        {
-            Debug.Log("Testing Chair");
-        }
-        
         public void TestChair(PlayerBase playerBase)
         {
-            Debug.Log("Testing .." + playerBase.name);
+            PlayerBase otherPlayer = PlayerQuery.players.FirstOrDefault(p => p != playerBase);
+            if(otherPlayer != null) Debug.Log($"Applying debuff from {playerBase.name} to: {otherPlayer.name}");
         }
     }
 }
