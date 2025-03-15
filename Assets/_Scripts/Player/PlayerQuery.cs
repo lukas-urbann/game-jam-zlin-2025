d47e87c6c63a35bf173e74d499d07580f76adb9a
Lukáš Urban <lukas.urbann@seznam.cz>
2025-03-15 17:29:19 +0100

    player query

---
+++ Assets/_Scripts/Player/PlayerQuery.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GJ25.Player
+{
+    public class PlayerQuery : MonoBehaviour
+    {
+        public static List<PlayerBase> players = new();
+    }
+}
+
