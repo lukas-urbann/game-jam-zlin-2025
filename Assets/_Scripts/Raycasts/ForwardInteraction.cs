4602de3e25088980c68a18bf1ae512cbf2c8b954
Lukáš Urban <lukas.urbann@seznam.cz>
2025-03-15 10:11:26 +0100

    udrzba

--- Assets/_Scripts/Raycasts/ForwardInteraction.cs
+++ Assets/_Scripts/Raycasts/ForwardInteraction.cs
@@ -1,21 +1,22 @@
 using UnityEngine;
 using GJ25.Interface;
+
 namespace GJ25.Raycasts
 {
-    public class InteractionFront : MonoBehaviour
+    public class ForwardInteraction : MonoBehaviour
     {
+        private PlayerBase player;
+        
         public float detectRayDistance = 1f;
         public Vector3 hitDir { get; private set; }
-        private PlayerBase player;
 
-        // uh oh... stinky...
-        private void Start()
+
+        private void OnEnable()
         {
-            if (player == null)
-                player = GetComponent<PlayerBase>();
+            if (TryGetComponent(out PlayerBase player)) this.player = player;
         }
 
-        private void Update()
+        private void FixedUpdate()
         {
             Vector3 playerForward = player.transform.forward;
             if (CheckDirection(playerForward))
@@ -26,18 +27,18 @@ namespace GJ25.Raycasts
 
         private bool CheckDirection(Vector3 direction)
         {
-            Ray ray = new Ray(transform.position, direction);
+            Ray ray = new(transform.position, direction);
             Debug.DrawRay(transform.position, direction * detectRayDistance, Color.red);
 
             if (Physics.Raycast(ray, out RaycastHit hit, detectRayDistance))
             {
+                if (player.State == PlayerBase.PlayerState.Moving) return false;
+
                 if (hit.collider.TryGetComponent(out IInteractable interactable))
                 {
-                    
                     Debug.DrawRay(transform.position, direction * detectRayDistance, Color.green);
                     return true;
                 }
-                
             }
 
             return false;
