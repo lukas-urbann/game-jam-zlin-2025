91c19c0f48ea47a84318128a9f65e93a1b50731b
vnmls <162630226+vnmls@users.noreply.github.com>
2025-03-15 15:34:37 +0100

    Add skibidi toilet

---
+++ Assets/_Scripts/Interactables/Box.cs
@@ -0,0 +1,13 @@
+using GJ25.Interactables;
+using UnityEngine;
+
+namespace  GJ25.Interactables
+{
+    public class Box : InteractableObjectBase
+    {
+        public void TestBox()
+        {
+            Debug.Log("Testing .." + this.name);
+        }
+    }
+}
--- Assets/_Scripts/Interactables/Chair.cs
+++ Assets/_Scripts/Interactables/Chair.cs
@@ -1,7 +1,18 @@
+using GJ25.Player;
+using UnityEngine;
+
 namespace GJ25.Interactables
 {
     public class Chair : InteractableObjectBase
     {
-
+        public void TestChair()
+        {
+            Debug.Log("Testing Chair");
+        }
+        
+        public void TestChair(PlayerBase playerBase)
+        {
+            Debug.Log("Testing .." + playerBase.name);
+        }
     }
 }
